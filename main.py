9bfa9b5ce0f986016fe68d82a6e80426f5202d26
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2020-12-14 10:53:00 +0700

    r

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 s=int(input('Срок размещения капитала (лет):'))
 k=int(input('Начальный капитал ($):'))
 p=int(input('Процентная ставка (%/мес.):'))
-i=int(input('Инвестиционные вливания ($/мес.):'))
-print('-'*57)
-print('|\t     \t|\t  основа  \t|\t сумма% \t|\t       \t|')
-print('|\tмесяц\t|\tинвестиций\t|\tза месяц\t|\tкапитал\t|')
-print('-'*57)
+a=int(input('Инвестиционные вливания ($/мес.):'))
+for year in range(1, s + 1):
+    print(year, 'год')
+    print('-' * 57)
+    print('|\t     \t|\t  основа  \t|\t сумма% \t|\t       \t|')
+    print('|\tмесяц\t|\tинвестиций\t|\tза месяц\t|\tкапитал\t|')
+    print('-' * 57)
+    if year == 1:
+        cp = k - 1000
+    for m in range(1, 13):
+        k = cp + 1000
+        sm = k * (p / 100)
+        cp = k + sm
+        print('|\t',m,'\t|\t',format(k,'15.,2f'),'\t|\t',format(sm,'15.,2f'),'\t|\t',format(cp,'15.,2f'),'\t|')
