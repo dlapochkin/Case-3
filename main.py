edd355cb58f38b6b55fc2f94a205fbf02859ae47
Лапочкин Дмитрий <d.lapochkin@g.nsu.ru>
2020-12-14 08:36:46 +0500

    push

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ s=int(input('Срок размещения капитала (лет):'))
 k=int(input('Начальный капитал ($):'))
 p=int(input('Процентная ставка (%/мес.):'))
 a=int(input('Инвестиционные вливания ($/мес.):'))
-print('-'*57)
-print('|\t     \t|\t  основа  \t|\t сумма% \t|\t       \t|')
-print('|\tмесяц\t|\tинвестиций\t|\tза месяц\t|\tкапитал\t|')
-print('-'*57)
-for i in range(1, s + 1):
-    for p in range(1, 13):
+for year in range(1, s + 1):
+    print(year, 'год')
+    print('-' * 57)
+    print('|\t     \t|\t  основа  \t|\t сумма% \t|\t       \t|')
+    print('|\tмесяц\t|\tинвестиций\t|\tза месяц\t|\tкапитал\t|')
+    print('-' * 57)
+    if year == 1:
+        cp = k - 1000
+        for m in range(1, 13):
+            k = cp + 1000
+            sm = k * (p / 100)
+            cp = k + sm
+            print('%.2f' % k,'%.2f' % sm,'%.2f' % cp)
+    else:
+        for m in range(1, 13):
+            k = cp + 1000
+            sm = k * (p / 100)
+            cp = k + sm
+            print('%.2f' % k, '%.2f' % sm, '%.2f' % cp)
