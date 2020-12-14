393ac4230cc806ca9f6a833b4b0991ed199f4036
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2020-12-14 12:47:18 +0700

    r

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ cp = k - 1000
 for year in range(1, s + 1):
     print(year, 'год')
     print('-' * 73)
-    print('|\t',''.ljust(5),'\t|\t','основа'.ljust(10),'\t|\t','сумма%'.ljust(10),'\t|\t',''.ljust(10),'\t|')
-    print('|\t','месяц','\t|\t','инвестиций'.ljust(10),'\t|\t','за месяц'.ljust(10),'\t|\t','капитал'.ljust(10),'\t|')
+    print('|\t',''.ljust(5),'\t|\t','основа'.ljust(10),'\t|\t','сумма%'.ljust(8),'\t|\t',''.ljust(10),'\t|')
+    print('|\tмесяц\t|\t','инвестиций'.ljust(10),'\t|\t','за месяц'.ljust(8),'\t|\t','капитал'.ljust(10),'\t|')
     print('-' * 73)
     for m in range(1, 13):
         k = cp + 1000
         sm = k * (p / 100)
         cp = k + sm
-        print('|\t', format(m,'2.0f'), '\t|\t', format(k, '10.2f'), '\t|\t', format(sm, '10.2f'), '\t|\t', format(cp, '10.2f'),'\t|')
+        print('|\t', format(m,'2.0f'), '\t|\t', format(k, '10.2f'), '\t|\t', format(sm, '8.2f'), '\t|\t', format(cp, '10.2f'),'\t|')
     print('-' * 73)
     print()
