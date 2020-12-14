e1639556699fe2a874e57a14598a7d3b8305abc9
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-12-14 10:42:46 +0700

    identify

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-print('something')
-sm = 0
-cp = 0
 s=int(input('Срок размещения капитала (лет):'))
 k=int(input('Начальный капитал ($):'))
 p=int(input('Процентная ставка (%/мес.):'))
 a=int(input('Инвестиционные вливания ($/мес.):'))
-print('-'*57)
-print('|\t     \t|\t  основа  \t|\t сумма% \t|\t       \t|')
-print('|\tмесяц\t|\tинвестиций\t|\tза месяц\t|\tкапитал\t|')
-print('-'*57)
-for p in range(1, 13):
-    k = cp + 1000
-    sm = k * p * 0.01
-    cp = k + sm
-    print(p, round(k, 2), round(sm, 2),round(kp, 2))
-
+for year in range(1, s + 1):
+    print(year, 'год')
+    print('-' * 57)
+    print('|\t     \t|\t  основа  \t|\t сумма% \t|\t       \t|')
+    print('|\tмесяц\t|\tинвестиций\t|\tза месяц\t|\tкапитал\t|')
+    print('-' * 57)
+    if year == 1:
+        cp = k - 1000
+        for m in range(1, 13):
+            k = cp + 1000
+            sm = k * (p / 100)
+            cp = k + sm
+            print('%.2f' % k,'%.2f' % sm,'%.2f' % cp)
+    else:
+        for m in range(1, 13):
+            k = cp + 1000
+            sm = k * (p / 100)
+            cp = k + sm
+            print('%.2f' % k, '%.2f' % sm, '%.2f' % cp)
