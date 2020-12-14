a290820ea70f4a198c60620e73efebf7886522c5
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-12-14 10:26:55 +0700

    identify

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+print('something')
+sm = 0
+kp = 0
 s=int(input('Срок размещения капитала (лет):'))
 k=int(input('Начальный капитал ($):'))
 p=int(input('Процентная ставка (%/мес.):'))
@@ -6,11 +9,8 @@ print('-'*57)
 print('|\t     \t|\t  основа  \t|\t сумма% \t|\t       \t|')
 print('|\tмесяц\t|\tинвестиций\t|\tза месяц\t|\tкапитал\t|')
 print('-'*57)
-if s == 1:
-    print('1 год')
-    cp = k - 1000
-    for m in range(1, 13):
-        k = cp + 1000
-        sm = k * (p / 100)
-        cp = k + sm
-        print('%.2f' % k,'%.2f' % sm,'%.2f' % cp)
+for p in range(1, 13):
+    sm = k * p * 0.01
+    kp = k + sm
+    print( p, k, sm, kp)
+    k = kp + a
