c084590b370dc68154bf64220c5db72d5c43da37
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2020-12-14 09:45:57 +0700

    table1

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ print('-'*57)
 print('|\t     \t|\t  основа  \t|\t сумма% \t|\t       \t|')
 print('|\tмесяц\t|\tинвестиций\t|\tза месяц\t|\tкапитал\t|')
 print('-'*57)
+for i in range (1,s+1):
+    for p in range(1,13):
